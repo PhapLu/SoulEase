ad3141ed77a70f64a8dcd9b71e24d8f0f6250e50
s4032012 <s4032012@rmit.edu.vn>
2025-11-30 01:10:06 +0700

    Complete Pricing page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -9,6 +9,7 @@ import Layout from "./Layout.jsx";
 import About from "./pages/About/About.jsx";
 import LandingPage from "./pages/LandingPage/LandingPage.jsx";
 import Services from "./pages/Services/Services.jsx";
+import Pricing from "./pages/Pricing/Pricing.jsx";
 import SignIn from "./pages/Auth/SignIn.jsx";
 import SignUp from "./pages/Auth/SignUp.jsx";
 import AuthLayout from "./pages/Auth/AuthLayout.jsx";
@@ -21,6 +22,7 @@ const router = createBrowserRouter([
       { index: true, element: <LandingPage /> },
       { path: "about", element: <About /> },
       { path: "services", element: <Services /> },
+      { path: "pricing", element: <Pricing /> },
     ],
   },
   { path: "/auth", 
