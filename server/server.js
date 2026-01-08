b813b1cc749bebdc29f8c26322d173032888ba05
Phap Lu <phapluudev2k5@gmail.com>
2026-01-08 13:21:39 +0700

    Update code

--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
 import server from './src/app.js'
 
 const PORT = process.env.PORT || 3052
-server.listen(process.env.PORT, () => {
+server.listen(PORT, () => {
     console.log(`Server is running on Port: ${PORT}`)
 })
